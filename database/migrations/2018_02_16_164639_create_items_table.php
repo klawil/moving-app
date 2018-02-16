b79220cda5fdb225f8f42282746bb299f39b2dba
William Klausmeyer <willyk95@gmail.com>
2018-02-16 10:53:28 -0600

    Add the items table

---
+++ database/migrations/2018_02_16_164639_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('box_id')->nullable();
+            $table->string('name', 100);
+            $table->string('description', 255)->nullable();
+            $table->string('barcode', 255)->nullable();
+            $table->string('model', 255)->nullable();
+            $table->string('serial', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
