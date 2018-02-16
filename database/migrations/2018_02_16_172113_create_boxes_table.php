13382637cf7e7b07d17939ddbb2afe9c274c1ed7
William Klausmeyer <willyk95@gmail.com>
2018-02-16 11:30:46 -0600

    Add the room and box tables

---
+++ database/migrations/2018_02_16_172113_create_boxes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBoxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boxes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('room_id')->nullable();
+            $table->integer('room_box_number')->nullable();
+            $table->string('description', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boxes');
+    }
+}
