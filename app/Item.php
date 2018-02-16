4ab2ec84d2294f076eb86300446ebebe9b584786
William Klausmeyer <willyk95@gmail.com>
2018-02-16 17:10:30 -0600

    Allow Item to be mass fillable

--- app/Item.php
+++ app/Item.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     /**
+     * The attributes that are not mass assignable
+     * @var Array
+     */
+    protected $guarded = ['id'];
+
+    /**
      * Returns the box the item is in
      * @return App\Box
      */
